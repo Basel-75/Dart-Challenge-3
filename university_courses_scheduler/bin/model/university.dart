ad77c71ed3d2846951d803607be0cb531f53ac2f
Basel-75 <basel_1422@outlook.com>
2024-08-08 14:33:20 +0300

    commit

--- university_courses_scheduler/bin/model/university.dart
+++ university_courses_scheduler/bin/model/university.dart
@@ -1,3 +1,4 @@
+import '../../data.dart';
 import 'courses.dart';
 
 class Univercity{
@@ -18,5 +19,20 @@ class Univercity{
     return {
       'courses' : courses
     };
+  }
+
+
+  addCourse(Courses course){
+   Data.data.addAll({'':course});
+  }
+
+  removeCourseByCode(String courseCode){
+    Data.data.removeWhere((key, value) => value==courseCode);
+  }
+  listAllCourses(){
+    Courses.fromJson(Data.data).toJson();
+  }
+  searchCourses(String query){
+    Data.data.forEach((key, value) { print(key.contains(query));});
   }
 }
